3b0bfb25c62a4e263b84f2b0866e1d453eb77cc3
Georgi Minchev <georgi.minchev@gmail.com>
2025-07-31 08:43:23 +0300

    add swaggeer

--- src/main/java/com/minchev/omni/entity/Scheduler.java
+++ src/main/java/com/minchev/omni/entity/Scheduler.java
@@ -1,9 +1,6 @@
 package com.minchev.omni.entity;
 
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
 import java.time.LocalDateTime;
 
 @Entity
